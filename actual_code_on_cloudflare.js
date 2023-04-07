238f32c0f2db6cdc0107b623575a91961b762be5
VKChabra <vkchabraww@gmail.com>
2023-04-07 19:08:42 +0300

    Create actual_code_on_cloudflare.js

---
+++ actual_code_on_cloudflare.js
@@ -0,0 +1,183 @@
+async function check_if_word_exists(userMsg, vocApi) {
+  const words = userMsg.toLowerCase().split(" ");
+  let index = 0;
+
+  if (words.length > 0 && words[0].length < 3) {
+    index = 1;
+  }
+  if (words.length > 1 && words[1]?.length < 3) {
+    index = 2;
+  }
+
+  const url =
+    "https://dictionaryapi.com/api/v3/references/sd3/json/" +
+    words[index] +
+    `?key=${vocApi}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (JSON.stringify(data).includes("meta")) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    // console.log(error);
+    return false;
+  }
+}
+
+const ukrainian = {
+  Q: "Й",
+  W: "Ц",
+  E: "У",
+  R: "К",
+  T: "Е",
+  Y: "Н",
+  U: "Г",
+  I: "Ш",
+  O: "Щ",
+  P: "З",
+  "{": "Х",
+  "}": "Ї",
+  A: "Ф",
+  S: "І",
+  D: "В",
+  F: "А",
+  G: "П",
+  H: "Р",
+  J: "О",
+  K: "Л",
+  L: "Д",
+  ":": "Ж",
+  '"': "Є",
+  Z: "Я",
+  X: "Ч",
+  C: "С",
+  V: "М",
+  B: "И",
+  N: "Т",
+  M: "Ь",
+  "<": "Б",
+  ">": "Ю",
+  "/": ".",
+  q: "й",
+  w: "ц",
+  e: "у",
+  r: "к",
+  t: "е",
+  y: "н",
+  u: "г",
+  i: "ш",
+  o: "щ",
+  p: "з",
+  "[": "х",
+  "]": "ї",
+  a: "ф",
+  s: "і",
+  d: "в",
+  f: "а",
+  g: "п",
+  h: "р",
+  j: "о",
+  k: "л",
+  l: "д",
+  ";": "ж",
+  "'": "є",
+  z: "я",
+  x: "ч",
+  c: "с",
+  v: "м",
+  b: "и",
+  n: "т",
+  m: "ь",
+  ",": "б",
+  ".": "ю",
+  "`": "'",
+  "?": ",",
+  // numbers 23467
+  $: ";",
+  "@": '"',
+  "#": "№",
+  "^": ":",
+  "&": "?",
+};
+
+function transliterate(input) {
+  let result = "";
+  for (let i = 0; i < input.length; i++) {
+    if (ukrainian[input[i]]) {
+      result += ukrainian[input[i]];
+    } else {
+      result += input[i];
+    }
+  }
+  return result;
+}
+
+const RegExpMsg = /^\S[a-zA-Z0-9\s[.\]{[}\];:"'<,>.?&/()!@#$%^*-_]*$/;
+const RegExpLink =
+  /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+const RegExpTelegramLink = /\b(?:https?:\/\/)?t\.me\/[a-zA-Z0-9_]+/;
+const RegExpNumber = /^[0-9,.-]+$/;
+
+function isValidMsg(userMsg) {
+  // should return true if msg is valid
+  if (
+    RegExpMsg.test(userMsg.toLowerCase()) &&
+    (!RegExpLink.test(userMsg.toLowerCase()) ||
+      !RegExpTelegramLink.test(userMsg.toLowerCase())) &&
+    !RegExpNumber.test(userMsg.toLowerCase())
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export default {
+  async fetch(request, env) {
+    if (request.method === "POST") {
+      try {
+        const reqBody = await request.json();
+        const userMsg = reqBody.message?.text?.toString();
+        const groupType = reqBody.message?.chat?.type?.toString();
+        if (!userMsg) {
+          // console.log(reqBody);
+          console.log("No user message");
+          return new Response(new String("No user message"), { status: 200 });
+        }
+        if (isValidMsg(userMsg)) {
+          if (
+            groupType !== "private" &&
+            (await check_if_word_exists(userMsg, env.VOCAB_API))
+          ) {
+            console.log(groupType, "should not translate");
+            return new Response(new String("Should not translate"), {
+              status: 200,
+            });
+          } else {
+            const chatId = reqBody.message?.chat.id;
+            const text = transliterate(userMsg);
+            const url = `https://api.telegram.org/bot${env.API_TOKEN}/sendMessage?chat_id=${chatId}&text=${text}`;
+            await fetch(url);
+          }
+        } else {
+          console.log("Not valid message");
+          return new Response(new String("Not valid message"), {
+            status: 200,
+          });
+        }
+        return new Response("KK");
+      } catch (error) {
+        // console.log(error);
+        return new Response(new String("Bad request"), { status: 400 });
+      }
+    } else {
+      return new Response(new String("Only POST methods are available"), {
+        status: 400,
+      });
+    }
+  },
+};
